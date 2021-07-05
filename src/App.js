53929da7ce1b052a400ffe8505a97ec38967fbd5
MartaRF3 <ricartfm@gmail.com>
2021-07-05 15:26:08 +0200

    Separated searcher into its own component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, { useState } from 'react';
 import './App.scss';
 
-import Current from "./components/current/Current";
+import Current from "./components/Current";
+import Searcher from "./components/Searcher";
 
 function App() {
 
   let [inputText, setInputText] = useState("");
   let [newCity, setNewCity] = useState("");
   
-  let inputTextHandler = (event) => {
-    setInputText(event.target.value);
-  }
-
-  let submitHandler = (event) => {
-    event.preventDefault();
-    setNewCity(inputText);
-  }
   return (
     <div className="wrapper">
     <div className="container">
-      <div className="left-container">
-        <form className="searcher" action="#" onSubmit={submitHandler}>
-          <input
-            onChange={inputTextHandler}
-            className="searcher__input"
-            type="text"
-            name="city"
-            id="city"
-            autoFocus="on"
-            autoComplete="off"
-            placeholder="Write a city here..."
-          />
-          <button className="searcher__submit" type="submit" id="submit">🔎</button>
-        </form>
-        {/* <button class="local-city">Your current location</button> */}
-      </div>
+      <Searcher inputText={inputText} setInputText={setInputText} setNewCity={setNewCity} />
       <Current cityName={newCity} />
     </div>
   </div>
---
+++ src/components/Searcher.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+let Searcher = ( {inputText, setInputText, setNewCity} ) => {
+
+  let inputTextHandler = (event) => {
+    setInputText(event.target.value);
+  }
+
+  let submitHandler = (event) => {
+    event.preventDefault();
+    setNewCity(inputText);
+    setInputText("");
+  }
+
+  <div className="left-container">
+    <form className="searcher" action="#">
+      <input
+        onChange={inputTextHandler}
+        className="searcher__input"
+        type="text"
+        name="city"
+        id="city"
+        autoFocus="on"
+        autoComplete="off"
+        placeholder="Write a city here..."
+        value={inputText}
+      />
+      <button 
+          onClick={submitHandler}
+          className="searcher__submit" 
+          type="submit" 
+          id="submit"
+      >🔎</button>
+    </form>
+    {/* <button class="local-city">Your current location</button> */}
+  </div>
+}
+
+export default Searcher;
